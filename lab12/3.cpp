b2553fdaa8faec6fb9298a91a585f6b84c3de52d
Idris <idris.makhmud8v@mail.ru>
2019-11-30 17:25:17 +0300

    upd

--- lab12/3.cpp
+++ lab12/3.cpp
@@ -5,19 +5,4 @@ using namespace std;
 
 void check_pass (string password)
 {
-    string valid_pass = "qwerty123";
-    if (password == valid_pass) {
-        cout << "Welcome!" << endl;
-    } else {
-        cout << "Poshel otsuda!" << endl;
-    }
-}
-
-int main()
-{
-    string user_pass;
-    cout << "Enter password: ";
-    getline (cin, user_pass);
-    check_pass (user_pass);
-    return 0;
-}
+    s
